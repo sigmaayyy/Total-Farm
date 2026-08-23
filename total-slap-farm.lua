d9fe549e117f89ca9271ae218db171ea35e957a7
sigmaayyy <vicedokpolor2@outlook.com>
2026-08-23 08:23:26 +0100

    Update total-slap-farm.lua

--- total-slap-farm.lua
+++ total-slap-farm.lua
@@ -1,1 +1,3 @@
+task.wait(3)
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NewNexer/NexerHub/refs/heads/main/SB/SFs/serverhop.luau"))({["snow"]=true;["slapples"]=true;["autoserverhop"]=6;})
