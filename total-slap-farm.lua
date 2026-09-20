34bf15c0fd5d9a63c2aa1a5ba8a386ff20f7aa6e
sigmaayyy <vicedokpolor2@outlook.com>
2026-09-20 16:36:45 +0100

    Update comments in total-slap-farm.lua
    Added comments regarding script ownership and cracking attempts.

--- total-slap-farm.lua
+++ total-slap-farm.lua
@@ -1,3 +1,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/lolledg/Sb/refs/heads/main/auto%20farm"))()
 
 -- this script was made by 6xow, my developer. on discord. If you were trying to skid anything here, good luck. 
+-- Also, for all those kids trying to crack my script, you can crack this one, we don't really care for this script
