885034b892c8774d49fb2c5906f524e6ee16e3d6
sigmaayyy <vicedokpolor2@outlook.com>
2026-09-20 16:45:10 +0100

    Update comment for open-source clarification

--- total-slap-farm.lua
+++ total-slap-farm.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NewNexer/NexerHub/refs/heads/main/SB/SFs/serverhop.luau"))({["snow"]=true;["slapples"]=true;["autoserverhop"]=6;}) 
--- for all those kids trying to crack my script, you can crack this one, we don't really care for this script. it's not even mine anyway lol
+-- for all those kids trying to crack my script, this one is open-sourced, we don't really care for this script. it's not even mine anyway lol
